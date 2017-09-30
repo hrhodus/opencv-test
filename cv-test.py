a096cb0ba390cd22f4d21f8a61b728c0856e2a58
hrhodus <hrhodus5@gmail.com>
2017-09-30 18:43:49 -0400

    show image from camera

--- cv-test.py
+++ cv-test.py
@@ -1,13 +1,21 @@
 import cv2
 cap = cv2.VideoCapture(0)
+print ("Input even for color livefeed")
+print ("Input odd for Grayscale livefeed")
+a = int(raw_input())
 
 while True:
     ret = False
     while ret == False:
         ret,frame = cap.read()
-    frame = cv2.flip(frame,flipCode=1)
-    cv2.imshow("frame",frame)
-    cv2.waitKey(1)
-
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if a % 2 == 0:
+        frame = cv2.flip(frame,flipCode=1)
+        cv2.imshow("frame",frame)
+        cv2.waitKey(1)
+    else:
+        frame = cv2.flip(frame,flipCode=1)
+        cv2.imshow("frame",gray)
+        cv2.waitKey(1)
 cap.release()
 cv2.destroyAllWindows()
